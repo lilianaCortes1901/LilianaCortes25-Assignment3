116349ae4f08b59c1a4fadfd403b382c720cfd56
Liliana Cortes <82052771+lilianaCortes1901@users.noreply.github.com>
2025-02-15 21:02:22 -0600

    Added DLNode class & Finished SongDLList.java
    Finished implementing SongDLList.java with SongDLNode file.

--- src/SongDLList.java
+++ src/SongDLList.java
@@ -1,24 +1,70 @@
 public class SongDLList {
-    String songTitle;
-    String artist;
-    double length;
+    private SongDLNode head;
+    private int size;
 
-    public SongDLList(String songTitle, String artist, double length){
-        this.songTitle = songTitle;
-        this.artist = artist;
-        this.length = length;
-        //SongDLList songDLList = new SongDLList();
+    public SongDLList(int maxSize){
+        this.head = null;
+        this.size = 0;
     }
 
-    public void addy(SongDLList songDLList){
+    public void addy(Song song){
+        SongDLNode newNode = new SongDLNode(song);
 
+        if(head == null){
+            head = newNode;
+        } else {
+            SongDLNode temp = head;
+            while (temp.next != null){
+                temp = temp.next;
+            }
+            temp.next = newNode;
+        }
+        size++;
     }
 
     public void removy(int pos){
-
+        if(pos == 0){
+            head = head.next;
+        } else {
+            SongDLNode temp = head;
+            for(int i = 0; i < pos -1; i++){
+                temp = temp.next;
+            }
+            temp.next = temp.next.next;
+        }
+        size--;
     }
 
     public String toString(){
-        return "Song[title: " + songTitle + ", artist: " + artist + ", length: " + length + "]";
+        StringBuilder sb = new StringBuilder();
+        SongDLNode temp = head;
+        while (temp != null){
+            sb.append(temp.data.toString()).append("\n");
+            temp = temp.next;
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args){
+        System.out.println("Cyberpunk Edgerunners Soundtrack: ");
+
+        SongDLList songList = new SongDLList(4);
+        Song song1 = new Song("Let You Down", "Dawid Podsiadlo", 3.47);
+        Song song2 = new Song("Who's Ready for Tomorrow", "Rat Boy, IBDY", 1.56);
+        Song song3 = new Song("This fffire", "Franz Ferdinand", 3.38);
+        Song song4 = new Song("I Really Want to Stay at Your House", "Rosa Walton, Hallie Coggins", 4.06);
+        Song song5 = new Song("Major Crimes", "Health", 4.13);
+
+        //add songs to list
+        songList.addy(song1);
+        songList.addy(song2);
+        songList.addy(song3);
+        songList.addy(song4);
+        songList.addy(song5); //increase list size
+
+        //remove song from list
+        songList.removy(3);
+
+        System.out.println(songList);
     }
 }
---
+++ src/SongDLNode.java
@@ -0,0 +1,9 @@
+public class SongDLNode {
+    Song data;
+    SongDLNode next;
+
+    public SongDLNode(Song data){
+        this.data = data;
+        this.next = null;
+    }
+}
